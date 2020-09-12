d6c681e03454bf5b6f4ada8105b033a5ee4ebfb3
oliverfab16 <fabriciocarvalho932@gmail.com>
2020-09-11 21:16:07 -0300

    cidades da baixada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cidades 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Ilha comprida");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Peruibe");
+            Console.WriteLine("Intanhaém");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Praia grande");
+            Console.WriteLine("São vicente");
+            Console.WriteLine("Santos");
+            Console.WriteLine("Cubatao");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("São sebastião");
+            Console.WriteLine("Caraguatatuba");
+            Console.WriteLine("Ilhabela");
+            Console.WriteLine("Ubatuba");
+        }
+    }
+}
